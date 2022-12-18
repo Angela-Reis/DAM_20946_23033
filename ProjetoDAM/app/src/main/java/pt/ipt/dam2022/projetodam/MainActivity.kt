5697f493fc70b2331a956854ae77a6cda0618ee0
MiguelModesto1 <101589792+MiguelModesto1@users.noreply.github.com>
2022-12-18 11:57:19 +0000

    Creation of branch for useful code

--- ProjetoDAM/app/src/main/java/pt/ipt/dam2022/projetodam/MainActivity.kt
+++ ProjetoDAM/app/src/main/java/pt/ipt/dam2022/projetodam/MainActivity.kt
@@ -1,11 +1,21 @@
 package pt.ipt.dam2022.projetodam
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * função genérica para mudar de view
+     */
+    fun changeActivity(view: View){
+        val intent = Intent(this, Test::class.java)
+        startActivity(intent)
     }
 }
